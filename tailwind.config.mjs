99eea0bad99ae5a73b1c62f224a9fe6713ba354b
Jocelyn-vnt <jocelyn.vinatie@etu.unilim.fr>
2024-11-26 15:42:45 +0100

    Infinite Scroll

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,26 @@ export default {
 				'anuphan': "Anuphan, sans-serif",
 				'alex' : "'Alex Brush', cursive",
 			  },
+			  backgroundImage: {
+				'KusmiTea': "url('/Kusmi-Tea.jpg')",
+				'Teaser': "url('/Teaser.png')",
+				'footer-texture': "url('/img/footer-texture.png')",
+			  },
+			  animation: {
+				"loop-scroll": 'loop-scroll 10s linear infinite',
+			  },
+			  keyframes: {
+				"loop-scroll": {
+				  from: { transform: 'translateY(0%)' },
+				  to:{ transform: 'translateY(-50%)' },
+				},
+			},	
+		
 		},
 	},
 	plugins: [],
+	safelist: [
+		'bg-KusmiTea',
+		'bg-Teaser',
+	  ],
 }
